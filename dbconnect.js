b3fb249e0d124469b5e3702612e99556b4d7526b
GabrielGregerson <Spidy700@hotmail.com>
2024-11-26 15:22:40 -0600

    Added more functionoality to MongoDB

---
+++ dbconnect.js
@@ -0,0 +1,12 @@
+const { mongoClient, ObjectId} = require('mongodb')
+
+const { uri } = require('./secrets/mongodb.json')
+
+const client = new MongoClient(uri)
+
+const GetFood = async (dbName, collectionName) => {
+    await client.connect()
+    return client.db(dbName).collection(collectionName)
+}
+
+module.exports = { GetFood, ObjectId }
