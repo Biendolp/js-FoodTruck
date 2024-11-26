09e8981159125eabbd38ecba92097a71afcb5f56
GabrielGregerson <Spidy700@hotmail.com>
2024-11-26 15:58:16 -0600

    Added more files

--- api/v1/Menu.js
+++ api/v1/Menu.js
@@ -1,1 +1,11 @@
 const router = require('express').router()
+const { response } = require('express')
+const {GetFood, ObjectId } = require('../../dbconnect')
+
+
+router.get('/:Number', async (request, response) => {
+    const { Number } = request.params
+    const collection = await GetFood('KrustyKartAPI', 'Menu Items')
+    console.log(collection.find().toArray())
+    response.send('done')
+})
