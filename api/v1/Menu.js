b5ee3906da39044420aa7ff31c313d39b2117a3f
GabrielGregerson <Spidy700@hotmail.com>
2024-12-10 17:58:32 -0600

    Removed code that was not being used

--- api/v1/Menu.js
+++ api/v1/Menu.js
@@ -2,21 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { GetCollection } = require('../../dbconnect');
 
-router.get('/:ProductName', async (request, response) => {
-    const { ProductName } = request.params;
+router.get('/:Number', async (request, response) => {
+    const { Number } = request.params;
     const collection = await GetCollection('KrustyKartAPI', 'Menu Items');
-    const found = await collection.findOne({ "ProductName": ProductName });
+    const found = await collection.findOne({ "Number": Number });
     response.json(found);
     
 });
 
-router.get('/:ID', async (request, response) => {
-    const { ID } = request.params;
-    const collection = await GetCollection('KrustyKartAPI', 'Events');
-    const found = await collection.findOne({ "ID": ID });
-    response.json(found);
-    
-});
+
 
 
 module.exports = router;
